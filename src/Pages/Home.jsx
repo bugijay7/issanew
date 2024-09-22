3e558e99fb6f5ba01f4ceb8669b520d948510751
bugijay7 <bugijay7@gmail.com>
2024-09-22 14:47:50 +0300

    Update and commit all files

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Testimonials from '../components/Testimonials';
 import About from '../components/About';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 function Home() {
   const [, setCount] = useState(0)
 
