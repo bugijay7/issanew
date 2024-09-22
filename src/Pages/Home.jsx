585127933b4f5a9e4b9bc96e02bfbf76104cc593
Kibugi Maina <138654192+bugijay7@users.noreply.github.com>
2024-09-22 15:07:57 +0300

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './home.css';
 
 import Banner from '../components/Banner';
@@ -6,11 +6,14 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Testimonials from '../components/Testimonials';
 import About from '../components/About';
+import Services from '../components/Services';  // Make sure to import this
+import Contacts from '../components/Contacts';  // Make sure to import this
+
 function Home() {
-  const [, setCount] = useState(0)
+  const [, setCount] = useState(0);
 
   return (
-      <div className='home-container'>
+    <div className='home-container'>
       <Navbar />
       <Banner />
       <About />
@@ -18,9 +21,8 @@ function Home() {
       <Testimonials />
       <Contacts />
       <Footer />
-      </div>
-    
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
