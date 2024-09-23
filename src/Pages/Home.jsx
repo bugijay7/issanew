7353e63cd17a344a94ae562de98301455012abc0
Kibugi Maina <138654192+bugijay7@users.noreply.github.com>
2024-09-23 03:01:22 +0300

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,14 +6,9 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Testimonials from '../components/Testimonials';
 import About from '../components/About';
-<<<<<<< HEAD
-import Services from '../components/Services';
-import Contacts from '../components/Contacts';
-=======
 import Services from '../components/Services';  // Make sure to import this
 import Contacts from '../components/Contacts';  // Make sure to import this
 
->>>>>>> c386fa1bbb80c889a4173f1f57aa995f6bc6b626
 function Home() {
   const [, setCount] = useState(0);
 
